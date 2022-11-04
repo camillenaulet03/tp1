53e0fad9483f4a510eda0d476761192fac19987f
kana03 <camille.naulet@hotmail.fr>
2022-11-04 10:16:11 +0100

    add pwa

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/sw.js");
+    navigator.serviceWorker.register("/sw.js")
+        .then((reg) => {
+            console.log("Enregistrement réussi");
+        }).catch((error) => {
+            console.log('erreur: '+error);
+    })
 }
