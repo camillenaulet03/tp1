7837d9164c71d92fc3beed429ee09faf2f948792
kana03 <camille.naulet@hotmail.fr>
2022-11-04 11:23:33 +0100

    complete sw

--- sw.js
+++ sw.js
@@ -1,14 +1,39 @@
-const staticCacheName = "cache-v1";
-const assets = ["/", "/index.html"];
+const staticCacheName = "my-site-cache-v1";
+const urlsToCache = ["/", "./style.css", './app.js', './index.js'];
 
 self.addEventListener("install", (e) => {
-    console.log("service install");
+    e.waitUntil(caches.open(staticCacheName).then((cache) => {
+        console.log('opened cache');
+        return cache.addAll(urlsToCache);
+    }));
 });
 
 self.addEventListener("fetch", (event) => {
-   console.log("récupérée "+event.request.url)
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            if (response) return response;
+
+            const fetchRequest = event.request.clone();
+
+            return fetch(fetchRequest).then(function (response) {
+                if (!response || response.status !== 200 || response.type !== "basic") return response;
+
+                const responseToCache = response.clone();
+                caches.open(staticCacheName).then(function (cache) {cache.put(event.request, responseToCache);});
+
+                return response;
+            });
+        })
+    );
 });
 
 self.addEventListener("activate", (e) => {
-    console.log("service activate");
+    e.waitUntil(caches.keys().then((cacheNames) => {
+        return Promise.all(
+            cacheNames.map(function(cacheName) {
+                if (staticCacheName.indexOf(cacheName) === -1)
+                    return caches.delete(cacheName);
+            })
+        );
+    }));
 });
